def036fdc55eefe385bf96ce869b46076c9db084
CmdrMahesh <mahesh@emptyops.com>
2019-11-17 20:22:53 +0530

    new feature: mysqldump and backup if skipped the backup-day

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ import tarfile
 import sys
 from dateutil import relativedelta
 import shutil
-
+import subprocess
 #######################################################################
 # make archive from source directory to the destination directory
 #######################################################################
@@ -21,7 +21,24 @@ def make_archive(src,dest,dest_dir):
     tar = tarfile.open(dest+'.tar.gz', "w:gz")
     tar.add(src)
     tar.close()
-    
+#######################################################################
+# function to check if command exists
+#######################################################################
+def is_tool(command):
+    return shutil.which(command) is not None
+#######################################################################
+# Dump database
+#######################################################################
+def db_dump(destination_dir,archive_parent,user,password):
+    dump_destination=(os.path.join(destination_dir,archive_parent,'DB'))
+    #make dirs if does not exists
+    os.makedirs(dump_destination, exist_ok=True)
+    if(is_tool('mysqldump')):
+        for db in open(os.path.join(os.path.dirname(__file__), "db.ini"), "r"):
+            db=db.strip()
+            os.popen('mysqldump --user=%s  --password=%s  --result-file="%s" --databases %s' % (user,password,os.path.join(dump_destination,"%s.sql"%db),db))
+    else:
+        print("Please add mysqldump to PATH environment variable")
 #######################################################################
 # maintainence code
 #######################################################################
@@ -44,9 +61,37 @@ def maintainence(dest):
                 shutil.rmtree(os.path.join(dest,dir_list.get(_dir)))
 
 #######################################################################
+# Function to determine the run
+#######################################################################
+def can_make_run(current_time):
+
+    #dir destination of config folder
+    config_dir=os.path.join(os.path.dirname(__file__),'config')
+    #make one if not exists our config dir
+    os.makedirs(config_dir, exist_ok=True)
+    #the file to hold last ran time stamp
+    last_run_config=os.path.join(os.path.dirname(__file__),'config/last_run')
+
+    #if current day is saturday return true else false
+    if(datetime.datetime.today().weekday() == 5):
+        return True
+    else:
+        if (not os.path.isfile(last_run_config)):
+            last_run_file=open(last_run_config,'w+')
+            last_run_file.write(str(int(current_time.timestamp())))
+            return True
+        else:
+            last_run_file=open(last_run_config,'r+')
+            last_run_time=last_run_file.readline().strip()
+            relative_time = int((current_time + relativedelta.relativedelta(days=-6)).timestamp())
+            if (int(relative_time) > int(last_run_time)):
+                return True
+            else:
+                return False
+
+#######################################################################
 # main execution - begins
 #######################################################################
-
 if (sys.platform == 'win32'):
     startup_path = os.path.join(os.path.expanduser('~'),
                                 'AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup')
@@ -68,19 +113,22 @@ else:
         exec /path/to/script.py
     '''
 
-#if(True):
-if(datetime.datetime.today().weekday()==5):
-    #current time
-    current_time=datetime.datetime.now() #date-month-year [hours-minute-sec]
+#current time
+current_time=datetime.datetime.now() #date-month-year [hours-minute-sec]
+db_user='root'
+db_password=''
+if(can_make_run(current_time)):
+    #if(True):
     #date formated directory name.
     archive_parent=current_time.strftime("%d-%m-%Y [%H-%M-%S]")
-
     for destination_dir in open(os.path.join(os.path.dirname(__file__),"destination.ini"),"r"):
         destination_dir=destination_dir.strip()
 
         for source_dir in open(os.path.join(os.path.dirname(__file__),"source.ini"),"r"):
             source_dir=source_dir.strip()
+
             if (not os.path.isdir(source_dir)):
                 continue
             make_archive(source_dir,destination_dir,archive_parent)
+            db_dump(destination_dir,archive_parent,user=db_user,password=db_password)
         maintainence(destination_dir)
